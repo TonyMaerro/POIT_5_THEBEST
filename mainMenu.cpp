2c5a493f7455f42197ea056cccc55555b9d134ab
Roman <95047534+demonchildd@users.noreply.github.com>
2021-12-14 00:28:45 +0300

    Add files via upload

--- mainMenu.cpp
+++ mainMenu.cpp
@@ -4,6 +4,37 @@ using namespace std;
 void main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
-
+	setlocale(LC_ALL, "ru");
+	do
+	{
+		cout << "Выберите игру" << endl;
+		cout << "1 - Змейка 12D" << endl;
+		cout << "2 - Стрелялка 2D" << endl;
+		cout << "3 - Угадать букву челлендж" << endl;
+		cout << "4 - Выход" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+		{
+			//Функция
+			break;
+		}
+		case 2:
+		{
+			//Функция
+			break;
+		}
+		case 3:  
+		{
+			//Функция
+			break;
+		}
+		case 4:
+		{
+			break;
+		}
+	} while (choice != 4);
+}
 	system("pause");
 }
